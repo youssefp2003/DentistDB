53466f44435b6e59bffddbfcd136658ac61da32c
youssef <youssef2003p@gmail.com>
2024-10-20 14:04:35 +0100

    Initial commit

--- app.py
+++ app.py
@@ -104,6 +104,45 @@ def live_search():
     ]
     return jsonify(results)
 
+@app.route('/edit_patient/<int:patient_id>', methods=['GET', 'POST'])
+def edit_patient(patient_id):
+    patient = Patient.query.get_or_404(patient_id)
+    if request.method == 'POST':
+        patient.nom = request.form['nom']
+        patient.prenom = request.form['prenom']
+        patient.telephone = request.form['telephone']
+        patient.numero_carte_national = request.form['numero_carte_national']
+        patient.assurance = request.form['assurance']
+        patient.profession = request.form['profession']
+        patient.maladie = request.form['maladie']
+        patient.observation = request.form['observation']
+        db.session.commit()
+        return redirect(url_for('patient_detail', patient_id=patient.id))
+    return render_template('edit_patient.html', patient=patient)
+
+@app.route('/delete_patient/<int:patient_id>', methods=['POST'])
+def delete_patient(patient_id):
+    patient = Patient.query.get_or_404(patient_id)
+    db.session.delete(patient)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/unpaid_balances')
+def unpaid_balances():
+    # Récupérer tous les patients avec des visites impayées
+    patients_with_unpaid = db.session.query(Patient).join(Visit).filter(Visit.reste > 0).distinct().all()
+    
+    # Calculer le total impayé pour chaque patient
+    patient_balances = []
+    for patient in patients_with_unpaid:
+        total_unpaid = sum(visit.reste for visit in patient.visits if visit.reste > 0)
+        patient_balances.append({
+            'patient': patient,
+            'total_unpaid': total_unpaid
+        })
+    
+    return render_template('unpaid_balances.html', patient_balances=patient_balances)
+
 # Add these lines at the end of your app.py file, just before if __name__ == '__main__':
 with app.app_context():
     # This will drop all existing tables
